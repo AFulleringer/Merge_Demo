fc66917dc0691e8aa851a8116b6650c25dcf2bb1
AlexF <alexfulleringer@gmail.com>
2025-03-17 17:15:18 -0400

    Adding some functions!

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+foo();
+bar();
+//A comment!
 
 void foo()
 {
@@ -9,4 +12,9 @@ void foo()
 void bar()
 {
     Console.WriteLine("This is bar");
+}
+
+void printHelloWorld()
+{
+    Console.WriteLine("Hello, World!")
 }
