bc7cf8b582eba5931240b8dad1e37f0c5b045133
AlexF <alexfulleringer@gmail.com>
2025-03-17 16:57:20 -0400

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+void foo()
+{
+    Console.WriteLine("This is foo");
+}
+
+void bar()
+{
+    Console.WriteLine("This is bar");
+}
